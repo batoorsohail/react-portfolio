718be9b5669e1f9bdd5101e80ddbf9a0ca78316b
unknown <sohailshahidi1435@gmail.com>
2023-10-23 16:38:47 +0430

    Add header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className="flex items-center justify-between bg-black px-3 py-7 md:py-20">
+      <a href="/"><h1 className="text-2xl font-bold text-white">Sohail <span className="p-1 bg-white text-black rounded-lg">Batoor</span></h1></a>
+    </header>
   )
 }
 
