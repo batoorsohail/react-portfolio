4bfd5d1a9e75472cbd37b7b9f5d73f8f6d8c95a1
unknown <sohailshahidi1435@gmail.com>
2023-10-24 21:45:40 +0430

    Update files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 const Navbar = () => {
   return (
-    <header className="flex items-center justify-between bg-black px-3 py-6 md:px-44">
+    <header className="flex items-center justify-between bg-black px-3 py-6 md:px-12 lg:px-44">
       <a href="/"><h1 className="text-2xl font-bold text-white">Sohail <span className="p-1 bg-white text-black rounded-lg">Batoor</span></h1></a>
-      <ul className="flex items-center justify-between text-white gap-14">
+      <ul className="hidden lg:flex items-center justify-between text-white gap-14">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
