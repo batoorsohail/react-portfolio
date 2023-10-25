63eca50a06cde85194c5bf9f58d622be96c957d7
unknown <sohailshahidi1435@gmail.com>
2023-10-25 19:01:51 +0430

    add light mode nav style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
+import Switcher from "./Switcher"
+
 const Navbar = () => {
   return (
-    <header className="flex items-center justify-between bg-black px-3 py-6 md:px-12 lg:px-44">
-      <a href="/"><h1 className="text-2xl font-bold text-white">Sohail <span className="p-1 bg-white text-black rounded-lg">Batoor</span></h1></a>
-      <ul className="hidden lg:flex items-center justify-between text-white gap-14">
+    <header className="flex items-center justify-between bg-black dark:bg-white px-3 py-6 md:px-12 lg:px-44">
+      <a href="/"><h1 className="text-2xl font-bold text-white dark:text-black">Sohail <span className="p-1 bg-white dark:bg text-black dark:text-white rounded-lg">Batoor</span></h1></a>
+      <ul className="hidden lg:flex items-center justify-between text-white dark:text-black gap-14">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
         <li>Portfolio</li>
         <li>Contact</li>
       </ul>
+      <Switcher />
     </header>
   )
 }
