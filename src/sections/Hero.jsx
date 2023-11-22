6e5fffa29a4102b2ec31540d9c6ee5f34f23bb34
unknown <sohailshahidi1435@gmail.com>
2023-11-22 21:44:46 +0430

    Add photo and details

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,18 @@
+import AboutPhoto from "../assets/about-img.jpg"
+
 const Hero = () => {
   return (
-    <div className="bg-black dark:bg-white h-[90vh]">Hero</div>
+    <section className="flex flex-col justify-center items-center">
+      <div>
+        <h1>Ahmad Sohail Batoor</h1>
+        <p>Full Stack Developer</p>
+        <p>A multi-skilled full-stack developer with hands-on experience in developing and maintaining web apps.</p>
+        <button type="button">Say Hello</button>
+      </div>
+      <div>
+        <img src={AboutPhoto} alt="Sohail photo" className="w-[180px] h-[180px] rounded-full" />
+      </div>
+    </section>
   )
 }
 
