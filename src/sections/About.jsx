fed7af4a24a9d080fc6fc9d4e5cb3eec0f6c9378
unknown <sohailshahidi1435@gmail.com>
2023-12-11 22:26:31 +0430

    Update About section style

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,30 +1,36 @@
 import AboutPhoto from '../assets/about-img.jpg';
+import { PiMedalBold } from "react-icons/pi";
+import { TbBooks } from "react-icons/tb";
 
 const About = () => {
   return (
-    <section className="bg-black text-white">
+    <section className="bg-black text-white px-5">
       <div className="flex flex-col items-center justify-center pt-8">
         <h2 className="text-white text-2xl font-bold mb-8">About Me</h2>
         <div>
           <img src={AboutPhoto} alt="Sohail's Photo" className="w-56 rounded-xl mb-10" />
         </div>
-        <div className="flex gap-2 mb-10">
-          <div className="flex flex-col items-center justify-center p-2 bg-[#333333] px-3 py-2 rounded-lg">
-            <p className="text-white text-[16px]">Experience</p>
+        <div className="flex gap-5 mb-10">
+          <div className="flex flex-col items-center justify-center p-2 bg-[#333333] px-3 py-4 rounded-lg">
+            <p className="text-2xl mb-2"><PiMedalBold /></p>
+            <p className="text-white text-lg">Experience</p>
             <p className="almost-gray">1+ year working</p>
           </div>
-          <div className="flex flex-col items-center justify-center p-2 bg-[#333333] px-3 py-2 rounded-lg">
-            <p className="text-white text-[16px]">Projects</p>
+          <div className="flex flex-col items-center justify-center p-2 bg-[#333333] px-3 py-4 rounded-lg">
+            <p className="text-2xl mb-2"><TbBooks /></p>
+            <p className="text-white text-lg">Projects</p>
             <p className="almost-gray">25+ completed</p>
           </div>
         </div>
-        <p className='almost-gray'>
-          Full-Stack Developer with a passion for pair programming, JavaScript, and remote work.
-          I have completed 30+ projects and received recognition for my work in software development. 
-          Currently, I am a Full-Stack Development student at Microverse, where I am honing my skills 
-          in front-end technologies such as JavaScript, React, and Redux, as well as back-end technologies
-          like PostgreSQL, Ruby, and Ruby on Rails.
-        </p>
+        <div>
+          <p className='almost-gray'>
+            Full-Stack Developer with a passion for pair programming, JavaScript, and remote work.
+            I have completed 30+ projects and received recognition for my work in software development. 
+            Currently, I am a Full-Stack Development student at Microverse, where I am honing my skills 
+            in front-end technologies such as JavaScript, React, and Redux, as well as back-end technologies
+            like PostgreSQL, Ruby, and Ruby on Rails.
+          </p>
+        </div>
       </div> 
     </section>
   )
