0e5b790a530c33bcc32b369b514ee070465f6c63
unknown <sohailshahidi1435@gmail.com>
2023-12-20 16:51:43 +0430

    Add Skills section style

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,24 +7,24 @@ const About = () => {
   return (
     <section className="bg-black dark:bg-white text-white px-5">
       <div className="flex flex-col items-center justify-center py-10">
-        <h2 className="text-white dark:text-black text-2xl font-bold mb-8">About Me</h2>
+        <h2 className="text-white dark:text-black text-2xl font-bold pb-8">About Me</h2>
         <div>
-          <img src={AboutPhoto} alt="Sohail's Photo" className="w-56 rounded-xl mb-10" />
+          <img src={AboutPhoto} alt="Sohail's Photo" className="w-56 rounded-xl pb-10" />
         </div>
-        <div className="flex justify-between w-full mb-10">
+        <div className="flex justify-between w-full pb-10">
           <div className="flex flex-col items-center justify-center bg-[#333333] p-4 rounded-lg">
-            <p className="text-2xl mb-2"><PiMedalBold /></p>
+            <p className="text-2xl pb-2"><PiMedalBold /></p>
             <p className="text-white text-lg">Experience</p>
             <p className="almost-gray">1+ year working</p>
           </div>
           <div className="flex flex-col items-center justify-center bg-[#333333] p-4 rounded-lg">
-            <p className="text-2xl mb-2"><TbBooks /></p>
+            <p className="text-2xl pb-2"><TbBooks /></p>
             <p className="text-white text-lg">Projects</p>
             <p className="almost-gray">25+ completed</p>
           </div>
         </div>
         <div>
-          <p className='almost-gray mb-8'>
+          <p className='almost-gray pb-8'>
             Full-Stack Developer with a passion for pair programming, JavaScript, and remote work.
             I have completed 30+ projects and received recognition for my work in software development. 
             Currently, I am a Full-Stack Development student at Microverse, where I am honing my skills 
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { FaHtml5, FaCss3Alt, FaReact } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
 
 const Skills = () => {
   return (
     <section>
-      <div class="flex flex-col items-center justify-center bg-black">
-        <h2 className="text-white text-3xl font-bold pt-10">Skills</h2>
-        <p className="almost-gray text-[18px]">Highlighting my abilities and expertise</p>
+      <div class="flex flex-col items-center justify-center bg-black px-5 py-10">
+        <h2 className="text-white text-3xl font-bold">Skills</h2>
+        <p className="almost-gray text-[18px] pb-8">Highlighting my abilities and expertise</p>
+        <div className="flex flex-wrap justify-center gap-5 w-full">
+          <div className="flex flex-col bg-[#323234] items-center justify-center w-[45%] gap-2 p-4 rounded-2xl">
+            <FaHtml5 className="text-white text-3xl"/>
+            <p className="text-white text-base">HTML</p>
+          </div>
+          <div className="flex flex-col bg-[#323234] items-center justify-center w-[45%] gap-2 p-4 rounded-2xl">
+            <FaCss3Alt className="text-white text-3xl" />
+            <p className="text-white text-base">CSS</p>
+          </div>
+          <div className="flex flex-col bg-[#323234] items-center justify-center w-[45%] gap-2 p-4 rounded-2xl">
+            <IoLogoJavascript className="text-white text-3xl"/>
+            <p className="text-white text-base">JavaScript</p>
+          </div>
+          <div className="flex flex-col bg-[#323234] items-center justify-center w-[45%] gap-2 p-4 rounded-2xl">
+            <FaReact className="text-white text-3xl" />
+            <p className="text-white text-base">React.js</p>
+          </div>
+        </div>
       </div>
     </section>
   )
