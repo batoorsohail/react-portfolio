4cf93efcf91a1e60ad27d16552b6026549efdd50
unknown <sohailshahidi1435@gmail.com>
2023-12-31 22:26:45 +0430

    Update style

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Navbar from './components/Navbar';
 import Hero from './sections/Hero';
 import About from './sections/About';
 import Skills from './sections/Skills'
+import Projects from './sections/Projects';
 
 const App = () => (
   <main className="h-screen">
     <Navbar />
     <Hero />
     <About />
+    <Projects />
     <Skills />
   </main>
 );
