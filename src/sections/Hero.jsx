d0061a0abd88d454b146c67f9d96008aa98b6cc5
unknown <sohailshahidi1435@gmail.com>
2024-01-06 23:13:40 +0430

    Update files

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className="flex md:items-center justify-center md:justify-between bg-black dark:bg-white h-[90vh] md:p-28">
       <div className="flex flex-col gap-2 p-5 mt-10 md:mt-0">
-        <p className="text-white">Hi there, I am</p>
+        <p className="text-white dark:text-black">Hi there, I am</p>
         <h1 className="text-5xl font-bold  text-white dark:text-black">Sohail Batoor</h1>
         <p className="text-[18px] lg:text-5xl font-bold text-white dark:text-black mb-3">
           <TypeAnimation
@@ -23,8 +23,8 @@ const Hero = () => {
           />
         </p>
         <div>
-          <p className="almost-gray text-base">A multi-skilled full-stack developer with hands-on experience in developing and maintaining web apps.</p>
-          <p className="almost-gray text-base mb-5">Currently, I am working as a self employed developer and I am looking for a full time job as a full stack developer.</p>
+          <p className="almost-gray text-base md:text-xl">A multi-skilled full-stack developer with hands-on experience in developing and maintaining web apps.</p>
+          <p className="almost-gray text-base md:text-xl mb-5">Currently, I am working as a self employed developer and I am looking for a full time job as a full stack developer.</p>
         </div>
         <button type="button" className="bg-white dark:bg-black text-[18px] text-black dark:text-white rounded-2xl w-40 py-4 px-5">Say Hello</button>
       </div>
