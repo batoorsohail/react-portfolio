b9cdcb5dce9e116bf78345e42334430210a16bdd
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-21 16:25:26 +0430

    Fix Skills.jsx bugs

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -2,7 +2,7 @@ const SkillCard = ({skill}) => {
   const { title, icon:Icon } = skill;
 
   return (
-    <div className="flex flex-col bg-[#202938] items-center w-[45%] md:flex-row md:w-[19%] gap-2 p-4 rounded-2xl">
+    <div className="flex flex-row bg-[#202938] items-center w-[90%] md:w-[30%] lg:w-[25%] gap-2 p-4 rounded-2xl">
       <div className="md:mr-4">
         <p>{Icon && <Icon className="text-coding-color text-3xl md:text-4xl" />}</p>
       </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,11 +5,11 @@ import SkillCard from "../components/SkillCard";
 const Skills = () => {
   return (
     <section id="skills">
-      <div class="flex flex-col items-center w-full h-[95vh] px-20 py-10">
+      <div class="flex flex-col items-center w-full h-auto md:h-[95vh] py-10">
         <BiChip className="text-coding-color text-4xl inline-block mb-4" />
         <h2 className="text-white text-3xl font-bold">Skills & Technologies</h2>
         <p className="text-white text-[18px] mt-3 mb-10">Highlighting my abilities and expertise</p>
-        <div className="flex flex-wrap justify-center gap-5 w-full">
+        <div className="flex flex-col md:flex-row flex-wrap items-center justify-center gap-5 w-full">
           {skills.map(skill => (
             <SkillCard key={skill.id} skill={skill} />
           ))}
