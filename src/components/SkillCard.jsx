0f41d47a6a0276654bdf0f65339263d7d4a31418
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-21 16:12:35 +0430

    Update Skills styling

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -2,9 +2,9 @@ const SkillCard = ({skill}) => {
   const { title, icon:Icon } = skill;
 
   return (
-    <div className="flex flex-col bg-[#323234] items-center w-[45%] md:flex-row md:w-[19%] gap-2 p-4 rounded-2xl">
+    <div className="flex flex-col bg-[#202938] items-center w-[45%] md:flex-row md:w-[19%] gap-2 p-4 rounded-2xl">
       <div className="md:mr-4">
-        <p>{Icon && <Icon className="text-white text-3xl md:text-4xl" />}</p>
+        <p>{Icon && <Icon className="text-coding-color text-3xl md:text-4xl" />}</p>
       </div>
       <div>
         <p className="text-white text-base md:text-xl md:font-bold">{title}</p>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,3 +1,4 @@
+import { BiChip } from "react-icons/bi";
 import { skills } from "../data";
 import SkillCard from "../components/SkillCard";
 
@@ -5,7 +6,8 @@ const Skills = () => {
   return (
     <section id="skills">
       <div class="flex flex-col items-center w-full h-[95vh] px-20 py-10">
-        <h2 className="text-white text-3xl font-bold">Skills</h2>
+        <BiChip className="text-coding-color text-4xl inline-block mb-4" />
+        <h2 className="text-white text-3xl font-bold">Skills & Technologies</h2>
         <p className="text-white text-[18px] mt-3 mb-10">Highlighting my abilities and expertise</p>
         <div className="flex flex-wrap justify-center gap-5 w-full">
           {skills.map(skill => (
